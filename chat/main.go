d279d9006669db7c252a4bd83aabb93d8131a0da
oldcorvus <moelcrow@gmail.com>
2023-02-26 19:54:40 +0100

    added log package

---
+++ chat/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"chicko_chat/log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("../templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
+
+	r := newRoom()
+	r.logger = logger.New(os.Stdout)
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
---
+++ chat/room.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"chicko_chat/log"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize,
+	WriteBufferSize: socketBufferSize}
+
+type room struct {
+	// channel that holds incoming messages
+	forward chan []byte
+
+	join chan *client
+
+	leave chan *client
+
+	clients map[*client]bool
+
+	logger logger.Logger
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			r.logger.Log("New Client Joined")
+
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			r.logger.Log("Client Left")
+
+		case msg := <-r.forward:
+			r.logger.Log("Message received: ", string(msg))
+			for client := range r.clients {
+				client.send <- msg
+				r.logger.Log("Sent to clients")
+			}
+		}
+	}
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
