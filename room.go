772bfc02c833c78f1ba571495d3b7f95d9335b36
oldcorvus <moelcrow@gmail.com>
2023-02-26 08:38:29 +0100

    functions for creating new room

--- room.go
+++ room.go
@@ -1,6 +1,71 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize,
+	WriteBufferSize: socketBufferSize}
+
 type room struct {
 	// channel that holds incoming messages
 	forward chan []byte
+
+	join chan *client
+
+	leave chan *client
+
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+
+		case msg := <-r.forward:
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
 }
