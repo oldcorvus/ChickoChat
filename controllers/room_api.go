d75a97cee26905daffafeaeb6eed95e5e04ec1c9
oldcorvus <moelcrow@gmail.com>
2023-03-10 16:17:47 +0100

    added CreateRoomApi endpoint for creating new room

---
+++ controllers/room_api.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"chicko_chat/models"
+	"time"
+)
+
+func (c *Controller) CreateRoom(ctx *gin.Context) {
+	// Validate input
+	var room *data.ChatRoom
+	if err := ctx.ShouldBindJSON(&room); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// Create room
+	room.CreatedAt = time.Now()
+	room, err := c.DB.CreateRoom(room)
+	if err == nil {
+		ctx.JSON(http.StatusOK, gin.H{"data": room})
+		return
+
+	}
+
+}
