176e0e1085164840b2f82481ba1246368d70eb2c
oldcorvus <moelcrow@gmail.com>
2023-03-12 10:03:48 +0100

    added endpoint for retrieving user details based on id for a room

--- controllers/user_api.go
+++ controllers/user_api.go
@@ -24,6 +24,26 @@ func (c *Controller) GetUserRoomsApi(ctx *gin.Context) {
 
 	}
 	ctx.JSON(http.StatusOK, gin.H{"data": rooms})
-
-
 }
+
+func (c *Controller) GetUserDetailsRoomApi(ctx *gin.Context) {
+	// Validate input
+	var room *data.ChatRoom
+	if err := ctx.ShouldBindJSON(&room); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var users []data.UserData
+	// Search For rooms
+	users, err := c.DB.GetUserData(room.Clients)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": users})
+}
+
+
+
