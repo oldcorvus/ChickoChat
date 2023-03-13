de66ada5c6d2071830ad7003c341b360e290c391
oldcorvus <moelcrow@gmail.com>
2023-03-13 11:31:52 +0100

    added method for wsServer for creating new broker

--- websocket/wsSerever.go
+++ websocket/wsSerever.go
@@ -20,3 +20,11 @@ func (server *WsServer) findBrokerbyRoomID(ID primitive.ObjectID) *Broker {
 	}
 	return nil 
 }
+
+func (server *WsServer) createBroker(room *ChatRoom) *Broker {
+	broker := NewBroker(room)
+	go broker.RunBroker()
+	server.brokers[broker] = true
+
+	return broker
+}
