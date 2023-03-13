86db4eb5decde70bfa2e102ca7a98077f222db80
oldcorvus <moelcrow@gmail.com>
2023-03-13 11:51:59 +0100

    added method serveWs for wsServer to handle websocket connection

--- websocket/wsSerever.go
+++ websocket/wsSerever.go
@@ -3,8 +3,19 @@ package websocket
 import (
 	"chicko_chat/database"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/gorilla/websocket"
+	"log"
 
 )
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize,
+	WriteBufferSize: socketBufferSize}
+
 
 type WsServer struct {
 	brokers      map[*data.Broker]bool
@@ -27,4 +38,35 @@ func (server *WsServer) createBroker(room *ChatRoom) *Broker {
 	server.brokers[broker] = true
 
 	return broker
+}
+
+func (server *WsServer)  ServeWs(w http.ResponseWriter, req *http.Request, roomId string, userId string ) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	roomId , err = data.ObjectIDFromHex()
+	userId , err = data.ObjectIDFromHex()
+
+	if err != nil {
+		return
+	}
+	user := &data.UserData{
+		ID : userId,
+	}
+
+	client := data.newClient(socket, user)
+	room := &data.ChatRoom{
+		ID :roomId,
+	}
+	broker := server.findBrokerbyRoomID(room)
+	if broker == nil {
+		server.createBroker(room)
+	}
+	broker.Clients[client] = true
+	broker.join <- client
+	defer func() { broker.leave <- client }()
+	go client.write()
+	go client.read()
 }
