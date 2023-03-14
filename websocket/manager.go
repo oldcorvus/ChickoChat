0de908bb8bcd4c49908d6d1ee2cf35d358b7ece0
oldcorvus <moelcrow@gmail.com>
2023-03-14 16:11:20 +0100

    added struc for BrokerManager

---
+++ websocket/manager.go
@@ -0,0 +1,35 @@
+package websocket
+
+import (
+	"chicko_chat/database"
+	"log"
+	"chicko_chat/models"
+
+	"github.com/gorilla/websocket"
+	"fmt"
+	"time"
+)
+
+const (
+	// Max wait time when writing message to peer
+	writeWait = 10 * time.Second
+
+	// Max time till next pong from peer
+	pongWait = 60 * time.Second
+
+	// Send ping interval, must be less then pong wait time
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 10000
+)
+
+// the amount of time to wait when pushing a message to
+// a slow client or a client that closed after `range Clients` started.
+const patience time.Duration = time.Second * 1
+
+type BrokerManager struct {
+	Brokers map[*data.Broker]bool
+	DB      *database.ChatDatabase
+}
+
